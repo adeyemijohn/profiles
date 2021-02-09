344c858c47e4c02456a1952130161d8e8ee55757
John Adeyemi <adeyemijohnayodele@gmail.com>
2021-02-09 22:12:08 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -28,3 +28,41 @@ const filterContainer=document.querySelector(".portfolio-filter"),
                 }
             })
         }
+
+        // Portfolio Lightbox
+        const lightbox=document.querySelector(".lightbox"),
+                lightboxImg=lightbox.querySelector(".lightbox-img"),
+                lightboxText=lightbox.querySelector(".caption-text"),
+                lightboxCounter=lightbox.querySelector(".caption-counter");
+        let itemIndex=0;
+
+        for ( let i=0; i<totalportfolioItem; i++){
+            portfolioItems[i].addEventListener("click", function(){
+                itemIndex=i;
+                changeItem();
+                toggleLightbox();
+            })
+        }
+
+        function nextItem(){
+            if (itemIndex == totalportfolioItem-1){
+                itemIndex=0;  
+            }
+            else{
+                itemIndex++
+            }
+            changeItem();
+        }
+
+        function toggleLightbox(){
+            lightbox.classList.toggle("open");
+        }
+
+        function changeItem(){
+            imgSrc=portfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+            lightbox.src=imgSrc;
+            lightboxText.innerHTML=portfolioItems[itemIndex].querySelector("h4").innerHTML;
+            lightboxCounter.innerHTML=(itemIndex+1) + " of " + totalportfolioItem;
+        };
+
+                
