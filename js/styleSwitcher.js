b4cea5d7f559a5609e78cc6d84320f8603848ca6
John Adeyemi <adeyemijohnayodele@gmail.com>
2021-02-13 06:12:31 +0100

    making changes to style switcher

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -13,6 +13,22 @@ function setActiveStyle(color){
     }
 }
 
+//body skin
+const bodySkin=document.querySelectorAll(".body-skin"),
+    totalBodySkin=bodySkin.length;
+
+    for (i=0; i<totalBodySkin; i++){
+        bodySkin[i].addEventListener("change", function(){
+            if (this.value === "dark"){
+                document.body.className="dark";
+            }
+            else{
+                document.body.className="";
+            }
+        })
+    }
+console.log(bodySkin);
+
 document.querySelector(".toggle-style-switcher").addEventListener("click", () =>{
     document.querySelector(".style-switcher").classList.toggle("open");
 })
