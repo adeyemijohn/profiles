c0620faa74726ce026cc1e196bc2f1752816c6b8
John Adeyemi <adeyemijohnayodele@gmail.com>
2021-02-13 08:03:26 +0100

    creating page active and layout

--- js/script.js
+++ js/script.js
@@ -80,3 +80,18 @@ const filterContainer=document.querySelector(".portfolio-filter"),
                 toggleLightbox();
             }
         })
+
+        //   Aside Nav
+        const nav=document.querySelector(".nav"),
+                navList=nav.querySelectorAll("li"),
+                totalNavList=navList.length;
+
+        for (let i=0; i<totalNavList; i++){
+            const a=navList[i].querySelector("a")
+            a.addEventListener("click", function(){
+                for (let j=0; j<totalNavList; j++){
+                    navList[j].querySelector("a").classList.remove("active")
+                }
+                this.classList.add("active");
+            })
+        }        
