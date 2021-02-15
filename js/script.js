294c21c45cd9bb5d752cf61283e3ff1bd478994c
John Adeyemi <adeyemijohnayodele@gmail.com>
2021-02-15 22:39:11 +0100

    trying to fix my position display

--- js/script.js
+++ js/script.js
@@ -92,9 +92,14 @@ const filterContainer=document.querySelector(".portfolio-filter"),
             const a=navList[i].querySelector("a")
             a.addEventListener("click", function(){
                 // remove back section class
+                for (let i=0; i<totalSection; i++){
+                allSection[i].classList.remove("back-section");
+            }
+
                 for (let j=0; j<totalNavList; j++){
-                    if(navList[j].querySelector("a").classList.contains("back-section")){
+                    if(navList[j].querySelector("a").classList.contains("active")){
                         // add back section class
+                        
                         allSection[j].classList.add("back-section");
                     }
                     navList[j].querySelector("a").classList.remove("active")
