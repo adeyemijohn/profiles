9ceb441cd90767fdaa98f6306c5161c71d7ee0fc
John Adeyemi <adeyemijohnayodele@gmail.com>
2021-02-16 13:26:01 +0100

    #contact was missing which make the button misbehaved and fixed

--- js/script.js
+++ js/script.js
@@ -98,6 +98,8 @@ const filterContainer=document.querySelector(".portfolio-filter"),
             const a=navList[i].querySelector("a")
             a.addEventListener("click", function(){
                 // remove back section class
+                removeBackSectionClass();
+
                 for (let i=0; i<totalSection; i++){
                 allSection[i].classList.remove("back-section");
             }
@@ -105,8 +107,7 @@ const filterContainer=document.querySelector(".portfolio-filter"),
                 for (let j=0; j<totalNavList; j++){
                     if(navList[j].querySelector("a").classList.contains("active")){
                         // add back section class
-                        
-                        allSection[j].classList.add("back-section");
+                        addBackSectionClass(j);
                     }
                     navList[j].querySelector("a").classList.remove("active")
                 }
@@ -118,7 +119,16 @@ const filterContainer=document.querySelector(".portfolio-filter"),
                 }
             })
         }
-        
+
+        function removeBackSectionClass(){
+            for (let i=0; i<totalSection; i++){
+                allSection[i].classList.remove("active");
+            }
+        }
+
+        function addBackSectionClass(num){
+            allSection[num].classList.add("back-section");
+        }
         function showSection(element){
             for (let i=0; i<totalSection; i++){
                 allSection[i].classList.remove("active");
@@ -132,14 +142,16 @@ const filterContainer=document.querySelector(".portfolio-filter"),
                 navList[i].querySelector("a").classList.remove("active");
                 const target=element.getAttribute("href").split("#")[1];
                 if(navList[i].querySelector("a").getAttribute("href").split("#")[1]){
-                    navList[i].querySelector("a").classList.add("active")
+                    navList[i].querySelector("a").classList.add("active");
                 }
             }
         }
 
         document.querySelector(".hire-me").addEventListener("click",function(){
+            const sectionIndex=this.getAttribute("data-section-index");
+            //console.log(sectionIndex);
             showSection(this);
-            updateNav(this);
+            updateNav(this); 
         })
 
 
